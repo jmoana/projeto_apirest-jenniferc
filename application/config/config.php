524dadd7ecbae220a882c34f0e92610bd9d3aa16
jmoana <jhenycruz7@gmail.com>
2021-12-13 23:52:42 -0300

    f

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https:/test148564.herokuapp.com/';
+$config['base_url'] = 'https:/oi.herokuapp.com/';
 //$config['base_url'] = 'https://localhost/projeto_apirest-jenniferc/';
 
 /*
